9b0ff7b236a4fcef70ff74589464c6759c9fe7de
a-h-sunny <amdadulhaquesunny@gmail.com>
2024-02-17 03:46:55 +0600

    final

--- script/script.js
+++ script/script.js
@@ -1,3 +1,48 @@
+function handleKeyupEvent(event){
+    const pressedKey = event.key;
+
+    const alphabet = document.getElementById('current-alphabet');
+    const currentAlphabet = alphabet.innerText;
+    const displaydAlphabet = currentAlphabet.toLowerCase();
+    if(pressedKey===displaydAlphabet){
+        console.log('you got a point')
+        removeBackgroundColor(displaydAlphabet);
+        const score = getElementValue('current-score');
+        const newScore = score + 1;
+        setElementValue('current-score',newScore);
+        
+        // -------------
+        // const currentScore = document.getElementById('current-score');
+        // const scoreValue = currentScore.innerText;
+        // const score = parseInt(scoreValue);
+        // const newScore = score + 1;
+        // currentScore.innerText = newScore;
+        continueGame();
+    }
+    else{
+        console.log('you lost a life')
+        removeBackgroundColor(displaydAlphabet);
+        const life = getElementValue('total-life');
+        const remainingLife = life - 1;
+        setElementValue('total-life',remainingLife);
+
+        if(remainingLife === 0){
+            hideElmentById('play-ground');
+            showElementById('score');
+            finalScore();
+        }
+
+        // ---------------------------
+        // const totalLife = document.getElementById('total-life');
+        // const lifeText = totalLife.innerText;
+        // const lifeNum = parseInt(lifeText);
+        // const remainingLife = lifeNum - 1;
+        // totalLife.innerText = remainingLife;
+        continueGame();
+    }
+}
+document.addEventListener('keyup',handleKeyupEvent);
+
 function continueGame(){
     const alphabet = getRandomAlphabet();
     const currentAlphabet = document.getElementById('current-alphabet');
@@ -16,5 +61,8 @@ function play(){
     // using utility function
     hideElmentById('home');
     showElementById('play-ground');
+    hideElmentById('score');
+    setElementValue('total-life',5);
+    setElementValue('current-score',0);
     continueGame();
  }
--- script/utility.js
+++ script/utility.js
@@ -20,3 +20,25 @@ function setBackgroundColor(element){
     const key = document.getElementById(element);
     key.classList.add('bg-orange-400');
 }
+function removeBackgroundColor(element){
+    const key = document.getElementById(element);
+    key.classList.remove('bg-orange-400');
+}
+function getElementValue(element){
+    const elementValue = document.getElementById(element);
+    const elementText = elementValue.innerText;
+    const value = parseInt(elementText);
+    return value;
+}
+function setElementValue(element,value){
+    const setValue = document.getElementById(element);
+    setValue.innerText = value;
+
+}
+function finalScore(){
+    const finalScore = document.getElementById('final-score');
+    const currentScore = document.getElementById('current-score');
+    const score = currentScore.innerText;
+    finalScore.innerText = score;
+    return score;
+}
